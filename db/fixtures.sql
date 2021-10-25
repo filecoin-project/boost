9103c29c299f73929d23bd625904a5903ddf1495
Dirk McCormick <dirkmdev@gmail.com>
2021-10-25 14:28:00 +0200

    feat: sketch of DB implementation

---
+++ db/fixtures.sql
@@ -0,0 +1,5 @@
+INSERT INTO Deals (ID, CreatedAt, PieceCID, PieceSize, Address, Client, Provider, Label, StartEpoch, EndEpoch, StoragePricePerEpoch, ProviderCollateral, ClientCollateral)
+VALUES
+('1', '2020-01-12T15:51:14.383', 'bafyreibnclzfntcs2iy7yhn3fvawv2lka4k7o5ac4n2jholpcqkvqoi4yu', 134217700, '12D3KooWNHFrhjRPafQrNK7h9zn5fAzMKDPszchvkVuEFM4EVXTW', 't1000', 't2000', 'f15s2bjbuj4d72heh5us2xghqrpam7z5cxqp7si6a', 519096, 519318, 4182000, 3210000, 0),
+('2', '2020-02-03T05:23:51.941', 'bafyreifzjh47bfbptr3wc2q2idjthnbfucud2dmv46rhl74fulrcnbhj54', 134247600, '12D3KooWQrZnhRaBecSZYKR815zH65xkg1TvfyPWUGYwehHvVEKB', 't1001', 't2000', 'bafyreiby3346thawnu7bwytjmwxr3obchlnuzt3xhbktbz6rdy2yxlqbhy', 512312, 514835, 4137000, 430000, 0)
+;
