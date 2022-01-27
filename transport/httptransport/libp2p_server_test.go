03c2a3b2f470c906298ec6d53c7351b4325c6f54
Dirk McCormick <dirkmdev@gmail.com>
2022-01-27 18:17:02 +0100

    feat: extract auth db from libp2p server

--- transport/httptransport/libp2p_server_test.go
+++ transport/httptransport/libp2p_server_test.go
@@ -2,8 +2,9 @@ package httptransport
 
 import (
 	"context"
+	"io"
+	"os"
 	"testing"
-	"time"
 
 	"github.com/filecoin-project/boost/transport/types"
 	"github.com/ipfs/go-cid"
@@ -13,8 +14,8 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// TestLibp2pDataServerAuth verifies that authorization works as expected
-func TestLibp2pDataServerAuth(t *testing.T) {
+// TestLibp2pCarServerAuth verifies that authorization works as expected
+func TestLibp2pCarServerAuth(t *testing.T) {
 	ctx := context.Background()
 
 	rawSize := 2 * 1024 * 1024
@@ -24,15 +25,15 @@ func TestLibp2pDataServerAuth(t *testing.T) {
 	defer srvHost.Close()
 	defer clientHost.Close()
 
-	srv := NewLibp2pCarServer(srvHost, st.ds, st.bs, ServerConfig{
+	authDB := NewAuthTokenDB(st.ds)
+	srv := NewLibp2pCarServer(srvHost, authDB, st.bs, ServerConfig{
 		AnnounceAddr: srvHost.Addrs()[0],
-		RetryTimeout: time.Minute,
 	})
 	err := srv.Start()
 	require.NoError(t, err)
 	defer srv.Stop() //nolint:errcheck
 
-	// Add an auth token
+	// Create an auth token
 	carSize := len(st.carBytes)
 	proposalCid, err := cid.Parse("bafkqaaa")
 	require.NoError(t, err)
@@ -42,10 +43,9 @@ func TestLibp2pDataServerAuth(t *testing.T) {
 
 	srvEvts := []types.TransferState{}
 	srv.Subscribe(func(sdbid uint, st types.TransferState) {
-		if dbid != sdbid {
-			return
+		if dbid == sdbid {
+			srvEvts = append(srvEvts, st)
 		}
-		srvEvts = append(srvEvts, st)
 	})
 
 	// Perform retrieval with the auth token
@@ -66,10 +66,10 @@ func TestLibp2pDataServerAuth(t *testing.T) {
 	lastSrvEvt := srvEvts[len(srvEvts)-1]
 	require.Equal(t, types.TransferStatusStarted, srvEvts[0].Status)
 	require.Equal(t, types.TransferStatusCompleted, lastSrvEvt.Status)
-	require.EqualValues(t, lastClientEvt.NBytesReceived, lastSrvEvt.Sent)
+	require.EqualValues(t, int(lastClientEvt.NBytesReceived), int(lastSrvEvt.Sent))
 
 	// Remove the auth token from the server
-	err = srv.Cleanup(xfer.AuthToken)
+	err = authDB.Delete(ctx, xfer.AuthToken)
 	require.NoError(t, err)
 
 	// Attempt a second retrieval - it should fail with a 401 HTTP error
@@ -82,120 +82,137 @@ func TestLibp2pDataServerAuth(t *testing.T) {
 	require.Error(t, evts2[len(evts2)-1].Error)
 }
 
+// TestLibp2pCarServerResume verifies that a transfer can resume from an
+// arbitrary place in the stream
+func TestLibp2pCarServerResume(t *testing.T) {
+	ctx := context.Background()
+
+	rawSize := 2 * 1024 * 1024
+	st := newServerTest(t, rawSize)
+
+	clientHost, srvHost := setupLibp2pHosts(t)
+	defer srvHost.Close()
+	defer clientHost.Close()
+
+	authDB := NewAuthTokenDB(st.ds)
+	srv := NewLibp2pCarServer(srvHost, authDB, st.bs, ServerConfig{
+		AnnounceAddr: srvHost.Addrs()[0],
+	})
+	err := srv.Start()
+	require.NoError(t, err)
+	defer srv.Stop() //nolint:errcheck
+
+	// Create an auth token
+	carSize := len(st.carBytes)
+	proposalCid, err := cid.Parse("bafkqaaa")
+	require.NoError(t, err)
+	dbid := uint(1)
+	xfer, err := srv.PrepareForDataRequest(context.Background(), dbid, proposalCid, st.root.Cid(), uint64(carSize))
+	require.NoError(t, err)
+
+	srvEvts := []types.TransferState{}
+	srv.Subscribe(func(sdbid uint, st types.TransferState) {
+		if dbid == sdbid {
+			srvEvts = append(srvEvts, st)
+		}
+	})
+
+	outFile := getTempFilePath(t)
+	retrieveData := func(readCount int, of string) {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+
+		// Perform retrieval with the auth token
+		req := newLibp2pHttpRequest(srvHost, xfer.AuthToken)
+		th := executeTransfer(t, ctx, New(clientHost), carSize, req, of)
+		require.NotNil(t, th)
+
+		// Wait for some data to be received by the client
+		clientSub := th.Sub()
+		for i := 0; i < readCount; i++ {
+			evt := <-clientSub
+			require.NoError(t, evt.Error)
+		}
+
+		// Disconnect the connection
+		err = clientHost.Network().ClosePeer(srvHost.ID())
+		require.NoError(t, err)
+
+		// Wait for the transfer to stall on the client
+		for {
+			select {
+			case evt := <-clientSub:
+				t.Logf("performed read till offset %d", evt.NBytesReceived)
+			default:
+				t.Logf("done reading")
+				return
+			}
+		}
+	}
+
+	truncateFromEnd := func(of string, fromEnd int) []byte {
+		f, err := os.Open(of)
+		require.NoError(t, err)
+
+		bz, err := io.ReadAll(f)
+		require.NoError(t, err)
+
+		err = f.Close()
+		require.NoError(t, err)
+
+		backtrack := min(len(bz), 1024)
+		err = os.WriteFile(outFile, bz[:len(bz)-backtrack], 0666)
+		require.NoError(t, err)
+
+		return bz
+	}
+
+	// Perform approximately one read (may read a few more times before the
+	// disconnect event propagates)
+	retrieveData(1, outFile)
+
+	// Remove the last 1k bytes from the file so that the next read will be
+	// from an earlier offset
+	truncateFromEnd(outFile, 1024)
+
+	// Do the same again
+	retrieveData(1, outFile)
+	truncateFromEnd(outFile, 1024)
+
+	// Now retrieve all bytes
+	req := newLibp2pHttpRequest(srvHost, xfer.AuthToken)
+	th := executeTransfer(t, ctx, New(clientHost), carSize, req, outFile)
+	require.NotNil(t, th)
+
+	// Wait for the transfer to complete
+	clientEvts := waitForTransferComplete(th)
+
+	// Check that all bytes were transferred successfully on the client
+	require.NotEmpty(t, clientEvts)
+	lastClientEvt := clientEvts[len(clientEvts)-1]
+	require.EqualValues(t, carSize, lastClientEvt.NBytesReceived)
+	assertFileContents(t, outFile, st.carBytes)
+
+	// Check that all bytes were transferred successfully on the server
+	require.NotEmpty(t, srvEvts)
+	lastSrvEvt := srvEvts[len(srvEvts)-1]
+	require.Equal(t, types.TransferStatusCompleted, lastSrvEvt.Status)
+	require.EqualValues(t, carSize, int(lastSrvEvt.Sent))
+}
+
+func min(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 func newLibp2pHttpRequest(h host.Host, token string) types.HttpRequest {
 	return types.HttpRequest{
 		URL: "libp2p://" + h.Addrs()[0].String() + "/p2p/" + h.ID().Pretty(),
 		Headers: map[string]string{
 			"Authorization": BasicAuthHeader("", token),
 		},
-	}
-}
-
-// TestLibp2pDataServerRetry verifies that there is an error event when the
-// client does not retry downloading the data before the retry timeout
-func TestLibp2pDataServerRetry(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	rawSize := 2 * 1024 * 1024
-
-	testCases := []struct {
-		name              string
-		retryTimeout      time.Duration
-		attemptRetryAfter time.Duration
-	}{{
-		name:              "client retries before timeout",
-		attemptRetryAfter: 100 * time.Millisecond,
-		retryTimeout:      1000 * time.Millisecond,
-	}, {
-		name:              "client retries after timeout",
-		attemptRetryAfter: 500 * time.Millisecond,
-		retryTimeout:      200 * time.Millisecond,
-	}}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			expectTimeout := tc.attemptRetryAfter < tc.retryTimeout
-			st := newServerTest(t, rawSize)
-
-			clientHost, srvHost := setupLibp2pHosts(t)
-			defer srvHost.Close()
-			defer clientHost.Close()
-
-			srv := NewLibp2pCarServer(srvHost, st.ds, st.bs, ServerConfig{
-				AnnounceAddr: srvHost.Addrs()[0],
-				RetryTimeout: tc.retryTimeout,
-			})
-			err := srv.Start()
-			require.NoError(t, err)
-			defer srv.Stop() //nolint:errcheck
-
-			// Add an auth token
-			carSize := len(st.carBytes)
-			proposalCid, err := cid.Parse("bafkqaaa")
-			require.NoError(t, err)
-			dbid := uint(1)
-			xfer, err := srv.PrepareForDataRequest(context.Background(), dbid, proposalCid, st.root.Cid(), uint64(carSize))
-			require.NoError(t, err)
-
-			// Perform retrieval with the auth token
-			req := newLibp2pHttpRequest(srvHost, xfer.AuthToken)
-			of := getTempFilePath(t)
-			backoff := BackOffRetryOpt(tc.attemptRetryAfter, tc.attemptRetryAfter, 1, 5)
-			ht := New(clientHost, backoff)
-
-			// Listen for server events
-			srvEvts := make(chan types.TransferState, rawSize)
-			srv.Subscribe(func(sdbid uint, st types.TransferState) {
-				if dbid != sdbid {
-					return
-				}
-				srvEvts <- st
-				if st.Status == types.TransferStatusCompleted || st.Status == types.TransferStatusFailed {
-					close(srvEvts)
-				}
-			})
-
-			th := executeTransfer(t, ctx, ht, carSize, req, of)
-			require.NotNil(t, th)
-
-			// Wait for some data to be received by the client
-			clientSub := th.Sub()
-			evt := <-clientSub
-			require.NoError(t, evt.Error)
-
-			// Disconnect the connection
-			err = clientHost.Network().ClosePeer(srvHost.ID())
-			require.NoError(t, err)
-
-			// Wait for the transfer to complete or error out on the client
-			var lastClientEvt types.TransportEvent
-			for evt := range clientSub {
-				lastClientEvt = evt
-			}
-
-			// Wait for the transfer to complete or error out on the server
-			var lastSrvEvt types.TransferState
-			for evt := range srvEvts {
-				lastSrvEvt = evt
-			}
-
-			if expectTimeout {
-				// The client is configured to retry before the retry timeout
-				// expires, so expect the transfer to succeed
-				require.NoError(t, lastClientEvt.Error)
-				require.Equal(t, types.TransferStatusCompleted, lastSrvEvt.Status)
-			} else {
-				// The client is configured to retry after the retry timeout
-				// expires, so expect the transfer to fail.
-				require.Error(t, lastClientEvt.Error)
-				// Expect a 401 error to occur because the auth token gets
-				// deleted when the retry timeout expires.
-				require.Contains(t, lastClientEvt.Error.Error(), "401")
-				// Expect a failure event on the server
-				require.Equal(t, types.TransferStatusFailed, lastSrvEvt.Status)
-			}
-		})
 	}
 }
 
