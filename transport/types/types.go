bea9f72ca7002a4c9c03087ea74fb369c7ce8dad
Dirk McCormick <dirkmdev@gmail.com>
2022-01-28 09:55:23 +0100

    refactor: simplify url parsing

--- transport/types/types.go
+++ transport/types/types.go
@@ -6,22 +6,34 @@ import (
 
 const DataTransferProtocol = "/fil/storage/transfer/1.0.0"
 
+// HttpRequest has parameters for an HTTP transfer
 type HttpRequest struct {
-	URL     string
+	// URL can be
+	// - an http URL:
+	//   "https://example.com/path"
+	// - a libp2p URL:
+	//   "libp2p:///ip4/104.131.131.82/tcp/4001/ipfs/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ"
+	//   Must include a Peer ID
+	URL string
+	// Headers are the HTTP headers that are sent as part of the request,
+	// eg "Authorization"
 	Headers map[string]string
 }
 
+// TransportDealInfo has parameters for a transfer to be executed
 type TransportDealInfo struct {
 	OutputFile string
 	DealUuid   uuid.UUID
 	DealSize   int64
 }
 
+// TransportEvent is fired as a transfer progresses
 type TransportEvent struct {
 	NBytesReceived int64
 	Error          error
 }
 
+// TransferStatus describes the status of a transfer (started, completed etc)
 type TransferStatus string
 
 const (
@@ -31,6 +43,7 @@ const (
 	TransferStatusFailed    TransferStatus = "TransferStatusFailed"
 )
 
+// TransferState describes a transfer's current state
 type TransferState struct {
 	LocalAddr  string
 	RemoteAddr string
