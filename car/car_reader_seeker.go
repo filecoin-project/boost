577cf4774fe8d84fea359a33ef0a84bc7cf9ff13
Dirk McCormick <dirkmdev@gmail.com>
2022-02-02 12:06:14 +0100

    fix: CarReaderSeeker deadlock

--- car/car_reader_seeker.go
+++ car/car_reader_seeker.go
@@ -8,65 +8,80 @@ import (
 )
 
 // CarReaderSeeker wraps CarOffsetWriter with a ReadSeeker implementation.
+// Note that the Read and Seek methods are not thread-safe. They must not
+// be called concurrently with each other, but may be called concurrently
+// with Cancel.
 type CarReaderSeeker struct {
-	parentCtx context.Context
-	size      uint64
-	offset    int64
-	cow       *CarOffsetWriter // 🐮
+	ctx    context.Context
+	cancel context.CancelFunc
+	size   uint64
+	offset int64
+	cow    *CarOffsetWriter // 🐮
 
-	lk            sync.Mutex
-	reader        *io.PipeReader
-	writer        *io.PipeWriter
-	writeCancel   context.CancelFunc
-	writeComplete chan struct{}
+	lk              sync.Mutex
+	reader          *io.PipeReader
+	writer          *io.PipeWriter
+	writeCompleteCh chan struct{}
 }
 
-var _ io.ReadSeekCloser = (*CarReaderSeeker)(nil)
+var _ io.ReadSeeker = (*CarReaderSeeker)(nil)
 
 func NewCarReaderSeeker(ctx context.Context, cow *CarOffsetWriter, size uint64) *CarReaderSeeker {
+	ctx, cancel := context.WithCancel(ctx)
 	return &CarReaderSeeker{
-		parentCtx:     ctx,
-		size:          size,
-		cow:           cow,
-		writeComplete: make(chan struct{}, 1),
+		ctx:    ctx,
+		cancel: cancel,
+		size:   size,
+		cow:    cow,
 	}
 }
 
-func (c *CarReaderSeeker) Read(p []byte) (n int, err error) {
+// Read reads data into the buffer. Not thread-safe to call concurrently with Seek.
+func (c *CarReaderSeeker) Read(p []byte) (int, error) {
 	c.lk.Lock()
-	defer c.lk.Unlock()
 
 	if uint64(c.offset) >= c.size {
+		defer c.lk.Unlock() // use defer so that we can read c.offset in error message
 		return 0, fmt.Errorf("cannot read from offset %d >= file size %d", c.offset, c.size)
 	}
 
+	// Check if the CarReadSeeker has been cancelled
+	if c.ctx.Err() != nil {
+		c.lk.Unlock()
+		return 0, c.ctx.Err()
+	}
+
 	// Check if there's already a write in progress
-	if c.reader == nil {
+	if c.writeCompleteCh == nil {
 		// No write in progress, start a new write from the current offset
 		// in a go routine
-		writeCtx, writeCancel := context.WithCancel(c.parentCtx)
-		c.writeCancel = writeCancel
+		c.writeCompleteCh = make(chan struct{})
+
 		pr, pw := io.Pipe()
 		c.reader = pr
 		c.writer = pw
 
 		go func() {
-			err := c.cow.Write(writeCtx, pw, uint64(c.offset))
-			if err != nil {
-				pw.CloseWithError(err) //nolint:errcheck
-			} else {
-				pw.Close() //nolint:errcheck
-			}
-			c.writeComplete <- struct{}{}
+			err := c.cow.Write(c.ctx, pw, uint64(c.offset))
+			pw.CloseWithError(err) //nolint:errcheck
+
+			c.lk.Lock()
+			defer c.lk.Unlock()
+
+			close(c.writeCompleteCh)
+			c.writeCompleteCh = nil
 		}()
 	}
+
+	// Don't hold the lock while reading as Read may block
+	c.lk.Unlock()
 
 	return c.reader.Read(p)
 }
 
+// Seek changes the offset into the stream. Not thread-safe to call concurrently with Read.
 func (c *CarReaderSeeker) Seek(offset int64, whence int) (int64, error) {
 	c.lk.Lock()
-	defer c.lk.Unlock()
 
 	// Update the offset
 	switch whence {
@@ -87,40 +102,52 @@ func (c *CarReaderSeeker) Seek(offset int64, whence int) (int64, error) {
 		c.offset = int64(c.size) + offset
 	}
 
-	// Cancel any ongoing write and wait for it to complete
-	if c.reader != nil {
-		c.writeCancel()
-
-		c.reader.Close() //nolint:errcheck
-
-		select {
-		case <-c.parentCtx.Done():
-			return 0, c.parentCtx.Err()
-		case <-c.writeComplete:
-		}
-
-		c.reader = nil
+	newOffset := c.offset
+	if c.writeCompleteCh == nil {
+		// No ongoing write so we can return immediately
+		c.lk.Unlock()
+		return newOffset, nil
 	}
 
-	return c.offset, nil
+	// There is an ongoing write, so close the pipe and wait for the write to
+	// complete before returning. This is so that subsequent reads will be from
+	// the updated offset.
+	// Note: Closing the reader will cause any subsequent reads / writes on
+	// that pipe to fail.
+	c.reader.Close() //nolint:errcheck
+
+	// Release the lock while waiting for write to complete
+	writeCompleteCh := c.writeCompleteCh
+	c.lk.Unlock()
+
+	select {
+	case <-c.ctx.Done():
+		return 0, c.ctx.Err()
+	case <-writeCompleteCh:
+	}
+
+	return newOffset, nil
 }
 
-func (c *CarReaderSeeker) Close() error {
+// Cancel aborts any read operation: Once Cancel returns, all subsequent calls
+// to Read() will return context.Canceled.
+func (c *CarReaderSeeker) Cancel() {
 	c.lk.Lock()
-	defer c.lk.Unlock()
 
-	if c.writer != nil {
-		return c.writer.Close()
+	// Cancel the context
+	c.cancel()
+
+	// Check if there's an ongoing write
+	writeCompleteCh := c.writeCompleteCh
+	if c.writeCompleteCh != nil {
+		// Close the writer. This will cause any subsequent reads to fail.
+		c.writer.CloseWithError(context.Canceled)
 	}
-	return nil
-}
 
-func (c *CarReaderSeeker) CloseWithError(err error) error {
-	c.lk.Lock()
-	defer c.lk.Unlock()
+	c.lk.Unlock()
 
-	if c.writer != nil {
-		return c.writer.CloseWithError(err)
+	// Wait for the write to complete
+	if writeCompleteCh != nil {
+		c.writeCompleteCh <- struct{}{}
 	}
-	return nil
 }
