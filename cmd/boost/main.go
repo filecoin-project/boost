a8b14ffab6b7cf99d334004981fcccf66e90095f
Dirk McCormick <dirkmdev@gmail.com>
2022-02-11 16:37:54 +0100

    feat: add ask v1 impl

--- cmd/boost/main.go
+++ cmd/boost/main.go
@@ -22,6 +22,11 @@ func main() {
 	_ = logging.SetLogLevel("gql", "INFO")
 	_ = logging.SetLogLevel("boost-provider", "INFO")
 	_ = logging.SetLogLevel("storagemanager", "INFO")
+	_ = logging.SetLogLevel("boost-storage-deal", "INFO")
+	_ = logging.SetLogLevel("boost-provider", "INFO")
+	_ = logging.SetLogLevel("http-transport", "INFO")
+	_ = logging.SetLogLevel("libp2p-server", "INFO")
+	_ = logging.SetLogLevel("boost-deal-publisher", "INFO")
 
 	app := &cli.App{
 		Name:                 "boost",
