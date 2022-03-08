f6f4be0425f2988324503282b2c1750f7f086acd
Aarsh Shah <aarshkshah1992@gmail.com>
2022-03-08 20:19:35 +0400

    index provider feedback 1

--- db/fixtures.go
+++ db/fixtures.go
@@ -56,7 +56,6 @@ func GenerateDeals() ([]types.ProviderDealState, error) {
 		return nil, err
 	}
 	publishCid := testutil.GenerateCid()
-	annCid := testutil.GenerateCid()
 
 	deals := []types.ProviderDealState{}
 	for _, clientNum := range clientAddrs {
@@ -96,13 +95,12 @@ func GenerateDeals() ([]types.ProviderDealState, error) {
 				Params: []byte(fmt.Sprintf("{url:'http://files.org/file%d.car'}", rand.Intn(1000))),
 				Size:   uint64(rand.Intn(10000)),
 			},
-			ChainDealID:            abi.DealID(rand.Intn(10000)),
-			PublishCID:             &publishCid,
-			IndexerAnnouncementCID: &annCid,
-			SectorID:               abi.SectorNumber(rand.Intn(10000)),
-			Offset:                 abi.PaddedPieceSize(rand.Intn(1000000)),
-			Length:                 abi.PaddedPieceSize(rand.Intn(1000000)),
-			Checkpoint:             dealcheckpoints.Accepted,
+			ChainDealID: abi.DealID(rand.Intn(10000)),
+			PublishCID:  &publishCid,
+			SectorID:    abi.SectorNumber(rand.Intn(10000)),
+			Offset:      abi.PaddedPieceSize(rand.Intn(1000000)),
+			Length:      abi.PaddedPieceSize(rand.Intn(1000000)),
+			Checkpoint:  dealcheckpoints.Accepted,
 		}
 
 		deals = append(deals, deal)
