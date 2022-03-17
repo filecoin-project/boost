28ee3144fa3dc48dd95c022efaf39080d63765fe
dirkmc <dirkmdev@gmail.com>
2022-03-17 13:55:01 +0800

    feat: better field names and messaging (#343)

--- react/src/Banner.js
+++ react/src/Banner.js
@@ -1,12 +1,15 @@
 import React from "react";
 import './Banner.css'
 
-export function ShowBanner(msg) {
+export function ShowBanner(msg, isError) {
     const banner = document.getElementById('banner')
     banner.classList.add('showing')
+    if (isError) {
+        banner.classList.add('error')
+    }
     setTimeout(function() {
         banner.classList.remove('showing')
-    }, 3000)
+    }, 5000)
 
     document.querySelector('#banner .message').textContent = msg
 }
