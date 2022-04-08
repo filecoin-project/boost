065bc17584cb6d72293e15f20170a4ab27c20129
Anton Evangelatov <anton.evangelatov@gmail.com>
2022-04-08 15:27:08 +0200

    enable market v1 deals; split itest for market v1 in two circleci jobs

--- itests/markets_v1_deal_test.go
+++ itests/markets_v1_deal_test.go
@@ -2,17 +2,13 @@ package itests
 
 import (
 	"context"
-	"fmt"
 	"io/ioutil"
 	"os"
-	"path/filepath"
 	"testing"
-	"time"
 
 	"github.com/filecoin-project/boost/itests/framework"
 	"github.com/filecoin-project/boost/testutil"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/lotus/api"
 	lapi "github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/itests/kit"
@@ -65,78 +61,6 @@ func TestMarketsV1Deal(t *testing.T) {
 	outPath := retrieve(t, f, ctx, dealProposalCid, res.Root, true)
 
 	log.Debugw("retrieval is done, compare in- and out- files", "in", inPath, "out", outPath)
-	kit.AssertFilesEqual(t, inPath, outPath)
-}
-
-func TestMarketsV1OfflineDeal(t *testing.T) {
-	ctx := context.Background()
-	log := framework.Log
-
-	kit.QuietMiningLogs()
-	framework.SetLogLevel()
-	framework.SetPreCommitChallengeDelay(t, 5)
-	f := framework.NewTestFramework(ctx, t, false)
-	err := f.Start()
-	require.NoError(t, err)
-	defer f.Stop()
-
-	// Create a CAR file
-	log.Debugw("using tempdir", "dir", f.HomeDir)
-
-	rseed := 1
-	size := 7 << 20 // 7MiB file
-	inPath, err := testutil.CreateRandomFile(f.HomeDir, rseed, size)
-	require.NoError(t, err)
-	res, err := f.FullNode.ClientImport(ctx, lapi.FileRef{Path: inPath})
-	require.NoError(t, err)
-
-	// Get the piece size and commP
-	rootCid := res.Root
-	pieceInfo, err := f.FullNode.ClientDealPieceCID(ctx, rootCid)
-	require.NoError(t, err)
-
-	// Create a new markets v1 deal
-	dp := f.DefaultMarketsV1DealParams()
-	dp.Data.Root = res.Root
-	// Replace with params for manual storage deal (offline deal)
-	dp.Data.TransferType = storagemarket.TTManual
-	dp.Data.PieceCid = &pieceInfo.PieceCID
-	dp.Data.PieceSize = pieceInfo.PieceSize.Unpadded()
-
-	log.Debugw("starting offline deal", "root", res.Root)
-	dealProposalCid, err := f.FullNode.ClientStartDeal(ctx, &dp)
-	require.NoError(t, err)
-	log.Debugw("got deal proposal cid", "cid", dealProposalCid)
-
-	// Wait for the deal to reach StorageDealCheckForAcceptance on the client
-	cd, err := f.FullNode.ClientGetDealInfo(ctx, *dealProposalCid)
-	require.NoError(t, err)
-	require.Eventually(t, func() bool {
-		cd, _ := f.FullNode.ClientGetDealInfo(ctx, *dealProposalCid)
-		fmt.Println(storagemarket.DealStates[cd.State])
-		return cd.State == storagemarket.StorageDealCheckForAcceptance
-	}, 60*time.Second, 500*time.Millisecond, "actual deal status is %s", storagemarket.DealStates[cd.State])
-
-	// Create a CAR file from the raw file
-	log.Debugw("generate out.car for miner")
-	carFilePath := filepath.Join(f.HomeDir, "out.car")
-	err = f.FullNode.ClientGenCar(ctx, api.FileRef{Path: inPath}, carFilePath)
-	require.NoError(t, err)
-
-	// Import the CAR file on the miner - this is the equivalent to
-	// transferring the file across the wire in a normal (non-offline) deal
-	log.Debugw("import out.car in boost")
-	err = f.Boost.MarketImportDealData(ctx, *dealProposalCid, carFilePath)
-	require.NoError(t, err)
-
-	log.Debugw("wait until offline deal is sealed")
-	err = f.WaitDealSealed(ctx, dealProposalCid)
-	require.NoError(t, err)
-
-	log.Debugw("offline deal is sealed, starting retrieval", "cid", dealProposalCid, "root", res.Root)
-	outPath := retrieve(t, nil, ctx, dealProposalCid, res.Root, true)
-
-	log.Debugw("retrieval of offline deal is done, compare in- and out- files", "in", inPath, "out", outPath)
 	kit.AssertFilesEqual(t, inPath, outPath)
 }
 
