a423aac17723d00640f0af4e760f85a9a9201544
Dirk McCormick <dirkmdev@gmail.com>
2022-04-15 12:21:08 +0200

    feat: update markets

--- go.mod
+++ go.mod
@@ -25,13 +25,15 @@ require (
 	github.com/filecoin-project/go-data-transfer v1.15.0
 	github.com/filecoin-project/go-fil-commcid v0.1.0
 	github.com/filecoin-project/go-fil-commp-hashhash v0.1.0
-	github.com/filecoin-project/go-fil-markets v1.20.2-0.20220325122707-b18483a0834b
+	// go-fil-markets boost/main branch
+	github.com/filecoin-project/go-fil-markets v1.20.2-0.20220415100246-742e1400623d
 	github.com/filecoin-project/go-jsonrpc v0.1.5
 	github.com/filecoin-project/go-padreader v0.0.1
 	github.com/filecoin-project/go-paramfetch v0.0.4
 	github.com/filecoin-project/go-state-types v0.1.3
 	github.com/filecoin-project/go-statestore v0.2.0
-	github.com/filecoin-project/index-provider v0.5.0
+	github.com/filecoin-project/index-provider v0.6.1
+	// lotus boost/main branch
 	github.com/filecoin-project/lotus v1.15.1-0.20220407132257-de8258c7a1bd
 	github.com/filecoin-project/specs-actors v0.9.14
 	github.com/filecoin-project/specs-actors/v2 v2.3.6
--- indexprovider/wrapper.go
+++ indexprovider/wrapper.go
@@ -23,7 +23,7 @@ import (
 	logging "github.com/ipfs/go-log/v2"
 
 	"github.com/filecoin-project/boost/storagemarket/types"
-	metadata2 "github.com/filecoin-project/index-provider/metadata"
+	"github.com/filecoin-project/index-provider/metadata"
 
 	"github.com/filecoin-project/boost/db"
 	lotus_storagemarket "github.com/filecoin-project/go-fil-markets/storagemarket"
@@ -108,7 +108,7 @@ func (w *Wrapper) Start(ctx context.Context) {
 		log.Errorw("failed to migrate dagstore indices for Boost deals", "err", err)
 	}
 
-	w.prov.RegisterCallback(func(ctx context.Context, contextID []byte) (provider.MultihashIterator, error) {
+	w.prov.RegisterMultihashLister(func(ctx context.Context, contextID []byte) (provider.MultihashIterator, error) {
 		provideF := func(pieceCid cid.Cid) (provider.MultihashIterator, error) {
 			ii, err := w.dagStore.GetIterableIndexForPiece(pieceCid)
 			if err != nil {
@@ -148,15 +148,12 @@ func (w *Wrapper) Start(ctx context.Context) {
 
 func (w *Wrapper) AnnounceBoostDeal(ctx context.Context, pds *types.ProviderDealState) (cid.Cid, error) {
 	// Announce deal to network Indexer
-	fm := metadata2.GraphsyncFilecoinV1Metadata{
+	fm := metadata.New(&metadata.GraphsyncFilecoinV1{
 		PieceCID:      pds.ClientDealProposal.Proposal.PieceCID,
 		FastRetrieval: true,
 		VerifiedDeal:  pds.ClientDealProposal.Proposal.VerifiedDeal,
-	}
-	dtm, err := fm.ToIndexerMetadata()
-	if err != nil {
-		return cid.Undef, fmt.Errorf("failed to encode indexer metadata: %w", err)
-	}
+	})
+
 	// ensure we have a connection with the full node host so that the index provider gossip sub announcements make their
 	// way to the filecoin bootstrapper network
 	if err := w.meshCreator.Connect(ctx); err != nil {
@@ -168,7 +165,7 @@ func (w *Wrapper) AnnounceBoostDeal(ctx context.Context, pds *types.ProviderDeal
 		return cid.Undef, fmt.Errorf("failed to get proposal cid from deal: %w", err)
 	}
 
-	annCid, err := w.prov.NotifyPut(ctx, propCid.Bytes(), dtm)
+	annCid, err := w.prov.NotifyPut(ctx, propCid.Bytes(), fm)
 	if err != nil {
 		return cid.Undef, fmt.Errorf("failed to announce deal to index provider: %w", err)
 	}
