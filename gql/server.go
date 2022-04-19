1d8e7fe71a122e94b81578a12de760d3edd68065
Dirk McCormick <dirkmdev@gmail.com>
2022-04-19 17:05:37 +0200

    fix: serve files over http from embedded fs

--- gql/server.go
+++ gql/server.go
@@ -2,23 +2,21 @@ package gql
 
 import (
 	"context"
+	_ "embed"
 	"fmt"
-	"io/ioutil"
+	"io/fs"
 	"net/http"
 	"os"
-	"path"
-	"path/filepath"
-	"runtime"
 	"strings"
 	"sync"
 	"time"
 
-	"golang.org/x/xerrors"
-
+	"github.com/filecoin-project/boost/react"
 	"github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
 	"github.com/graph-gophers/graphql-transport-ws/graphqlws"
 	logging "github.com/ipfs/go-log/v2"
+	"golang.org/x/xerrors"
 )
 
 var log = logging.Logger("gql")
@@ -35,16 +33,15 @@ func NewServer(resolver *resolver) *Server {
 	return &Server{resolver: resolver}
 }
 
+//go:embed schema.graphql
+var schemaGraqhql string
+
 func (s *Server) Start(ctx context.Context) error {
 	log.Info("graphql server: starting")
 
-	// Get the absolute path to the directory that this go file is in
-	_, goFilePath, _, ok := runtime.Caller(0)
-	goFileDir := filepath.Dir(goFilePath)
-
 	// Serve React app
 	mux := http.NewServeMux()
-	err := serveReactApp(mux, goFileDir)
+	err := serveReactApp(mux)
 	if err != nil {
 		return err
 	}
@@ -58,21 +55,10 @@ func (s *Server) Start(ctx context.Context) error {
 	// GraphQL handler (GUI for making GraphQL queries)
 	mux.HandleFunc("/graphiql", graphiql(httpPort))
 
-	// Init GraphQL schema
-	if !ok {
-		return fmt.Errorf("couldnt call runtime.Caller")
-	}
-	fpath := filepath.Join(goFileDir, "schema.graphql")
-
-	schemaText, err := ioutil.ReadFile(fpath)
-	if err != nil {
-		return err
-	}
-
 	// Allow resolving directly to fields (instead of requiring resolvers to
 	// have a method for every GraphQL field)
 	opts := []graphql.SchemaOpt{graphql.UseFieldResolvers()}
-	schema, err := graphql.ParseSchema(string(schemaText), s.resolver, opts...)
+	schema, err := graphql.ParseSchema(string(schemaGraqhql), s.resolver, opts...)
 	if err != nil {
 		return err
 	}
@@ -105,25 +91,42 @@ func (s *Server) Start(ctx context.Context) error {
 	return nil
 }
 
-func serveReactApp(mux *http.ServeMux, goFileDir string) error {
+// fsPrefix adds a prefix to all Open() calls
+type fsPrefix struct {
+	fs.FS
+	prefix string
+}
+
+var _ fs.FS = (*fsPrefix)(nil)
+
+func (f *fsPrefix) Open(name string) (fs.File, error) {
+	if name == "." || name == "" {
+		return f.FS.Open(f.prefix)
+	}
+	return f.FS.Open(f.prefix + "/" + name)
+}
+
+func serveReactApp(mux *http.ServeMux) error {
 	// Catch all requests that are not handled by other handlers
 	urlPath := "/"
 
-	// Path from root of project to static react build
-	directory := "react/build"
-	reactDir := path.Clean(filepath.Join(goFileDir, "..", directory))
-	reactFiles, err := ioutil.ReadDir(reactDir)
+	reactFiles, err := react.BuildDir.ReadDir("build")
 	if err != nil {
 		if !xerrors.Is(err, os.ErrNotExist) {
-			return fmt.Errorf("reading files in react dir '%s': %w", reactDir, err)
+			return fmt.Errorf("reading files in embedded react build dir: %w", err)
 		}
 
-		log.Warnw("not serving react app: no files found in react directory", "directory", directory)
+		log.Warnw("not serving react app: no files found in embedded react build directory - " +
+			"you may need to build the boost react app")
 		return nil
 	}
 
-	// Serves files in the react dir
-	reactApp := http.StripPrefix(urlPath, http.FileServer(http.Dir(reactDir)))
+	// Serves files in the react build dir
+	reactFS := &fsPrefix{
+		FS:     react.BuildDir,
+		prefix: "build",
+	}
+	reactApp := http.StripPrefix(urlPath, http.FileServer(http.FS(reactFS)))
 
 	mux.HandleFunc(urlPath, func(writer http.ResponseWriter, request *http.Request) {
 		matchesFile := func() bool {
