ad6ad3d043fd948f6ae11d58a92fb6aa82d30b22
LexLuthr <88259624+LexLuthr@users.noreply.github.com>
2022-06-28 11:57:29 +0530

    add net commands (#614)
    * add net commands and methods
    
    * docsgen
    
    * fix lint errors. change dtypes.ScoreKeeper
    
    * fix lint error. Switch to lotus scorekeeper
    
    * fix lint errors
    
    * Add Net command to main
    
    * refactor: import lotus net implementation instead of copying it
    
    * refactor: copy ResourceManager from lotus modules
    
    * refactor: lowercase NetCmd
    
    Co-authored-by: Dirk McCormick <dirkmdev@gmail.com>

--- api/net.go
+++ api/net.go
@@ -2,7 +2,10 @@ package api
 
 import (
 	"context"
+	"time"
 
+	lotus_api "github.com/filecoin-project/lotus/api"
+	lotus_net "github.com/filecoin-project/lotus/node/impl/net"
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
@@ -20,18 +23,22 @@ import (
 //  * Generate markdown docs
 //  * Generate openrpc blobs
 
+// This interface is a direct copy of the lotus equivalent.
+// We can't just include the lotus equivalent directly because of the way that
+// the generator works (it doesn't pull in types that are outside this directory)
 type Net interface {
 	// MethodGroup: Net
-	NetConnectedness(context.Context, peer.ID) (network.Connectedness, error) //perm:read
-	NetPeers(context.Context) ([]peer.AddrInfo, error)                        //perm:read
-	NetConnect(context.Context, peer.AddrInfo) error                          //perm:write
-	NetAddrsListen(context.Context) (peer.AddrInfo, error)                    //perm:read
-	NetDisconnect(context.Context, peer.ID) error                             //perm:write
-	NetFindPeer(context.Context, peer.ID) (peer.AddrInfo, error)              //perm:read
-	//NetPubsubScores(context.Context) ([]PubsubScore, error)                   //perm:read
-	NetAutoNatStatus(context.Context) (NatInfo, error)               //perm:read
-	NetAgentVersion(ctx context.Context, p peer.ID) (string, error)  //perm:read
-	NetPeerInfo(context.Context, peer.ID) (*ExtendedPeerInfo, error) //perm:read
+	NetConnectedness(context.Context, peer.ID) (network.Connectedness, error)  //perm:read
+	NetPeers(context.Context) ([]peer.AddrInfo, error)                         //perm:read
+	NetPing(context.Context, peer.ID) (time.Duration, error)                   //perm:read
+	NetConnect(context.Context, peer.AddrInfo) error                           //perm:write
+	NetAddrsListen(context.Context) (peer.AddrInfo, error)                     //perm:read
+	NetDisconnect(context.Context, peer.ID) error                              //perm:write
+	NetFindPeer(context.Context, peer.ID) (peer.AddrInfo, error)               //perm:read
+	NetPubsubScores(context.Context) ([]lotus_api.PubsubScore, error)          //perm:read
+	NetAutoNatStatus(context.Context) (lotus_api.NatInfo, error)               //perm:read
+	NetAgentVersion(ctx context.Context, p peer.ID) (string, error)            //perm:read
+	NetPeerInfo(context.Context, peer.ID) (*lotus_api.ExtendedPeerInfo, error) //perm:read
 
 	// NetBandwidthStats returns statistics about the nodes total bandwidth
 	// usage and current rate across all peers and protocols.
@@ -46,12 +53,20 @@ type Net interface {
 	NetBandwidthStatsByProtocol(ctx context.Context) (map[protocol.ID]metrics.Stats, error) //perm:read
 
 	// ConnectionGater API
-	NetBlockAdd(ctx context.Context, acl NetBlockList) error    //perm:admin
-	NetBlockRemove(ctx context.Context, acl NetBlockList) error //perm:admin
-	NetBlockList(ctx context.Context) (NetBlockList, error)     //perm:read
+	NetBlockAdd(ctx context.Context, acl lotus_api.NetBlockList) error    //perm:admin
+	NetBlockRemove(ctx context.Context, acl lotus_api.NetBlockList) error //perm:admin
+	NetBlockList(ctx context.Context) (lotus_api.NetBlockList, error)     //perm:read
+	NetProtectAdd(ctx context.Context, acl []peer.ID) error               //perm:admin
+	NetProtectRemove(ctx context.Context, acl []peer.ID) error            //perm:admin
+	NetProtectList(ctx context.Context) ([]peer.ID, error)                //perm:read
 
 	// ID returns peerID of libp2p node backing this API
 	ID(context.Context) (peer.ID, error) //perm:read
+
+	// ResourceManager API
+	NetStat(ctx context.Context, scope string) (lotus_api.NetStat, error)          //perm:read
+	NetLimit(ctx context.Context, scope string) (lotus_api.NetLimit, error)        //perm:read
+	NetSetLimit(ctx context.Context, scope string, limit lotus_api.NetLimit) error //perm:admin
 }
 
 type CommonNet interface {
@@ -59,7 +74,4 @@ type CommonNet interface {
 	Net
 }
 
-type NatInfo struct {
-	Reachability network.Reachability
-	PublicAddr   string
-}
+var _ Net = (*lotus_net.NetAPI)(nil)
--- cmd/boostd/main.go
+++ cmd/boostd/main.go
@@ -47,6 +47,7 @@ func main() {
 			logCmd,
 			dagstoreCmd,
 			piecesCmd,
+			netCmd,
 		},
 	}
 	app.Setup()
