0d9c2d62195fd895cd408d7dfb3ce05f8168ff7f
Anton Evangelatov <anton.evangelatov@gmail.com>
2022-06-29 12:00:59 +0200

    running boost wip

--- cmd/boost/provider_cmd.go
+++ cmd/boost/provider_cmd.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"sort"
 	"strings"
+	"sync"
 
 	bcli "github.com/filecoin-project/boost/cli"
 	"github.com/filecoin-project/boost/cli/ctxutil"
@@ -28,9 +29,135 @@ var providerCmd = &cli.Command{
 	Usage: "Info about Storage Providers",
 	Flags: []cli.Flag{cmd.FlagRepo},
 	Subcommands: []*cli.Command{
+		runningBoostCmd,
 		libp2pInfoCmd,
 		storageAskCmd,
 		retrievalAskCmd,
+	},
+}
+
+var runningBoostCmd = &cli.Command{
+	Name:        "running-boost",
+	Usage:       "",
+	ArgsUsage:   "",
+	Description: "",
+	Before:      before,
+	Action: func(cctx *cli.Context) error {
+		ctx := ctxutil.ReqContext(cctx)
+
+		n, err := clinode.Setup(cctx.String(cmd.FlagRepo.Name))
+		if err != nil {
+			return fmt.Errorf("setting up CLI node: %w", err)
+		}
+
+		api, closer, err := lcli.GetGatewayAPI(cctx)
+		if err != nil {
+			return fmt.Errorf("setting up gateway connection: %w", err)
+		}
+		defer closer()
+
+		miners, err := api.StateListMiners(ctx, types.EmptyTSK)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("got len total miners: ", len(miners))
+
+		var wg sync.WaitGroup
+		wg.Add(len(miners))
+		var lk sync.Mutex
+		var withMinPower []address.Address
+
+		throttle := make(chan struct{}, 50)
+		for _, miner := range miners {
+			throttle <- struct{}{}
+			go func(miner address.Address) {
+				defer wg.Done()
+				defer func() {
+					<-throttle
+				}()
+
+				power, err := api.StateMinerPower(ctx, miner, types.EmptyTSK)
+				if err != nil {
+					panic(err)
+				}
+
+				if power.HasMinPower { // TODO: Lower threshold
+					lk.Lock()
+					withMinPower = append(withMinPower, miner)
+					lk.Unlock()
+				}
+			}(miner)
+		}
+
+		wg.Wait()
+
+		fmt.Println("got len miners with min power: ", len(miners))
+
+		for _, maddr := range withMinPower {
+			select {
+			case <-ctx.Done():
+				return nil
+			default:
+			}
+			err := func() error {
+				addrInfo, err := cmd.GetAddrInfo(ctx, api, maddr)
+				if err != nil {
+					return fmt.Errorf("getting provider multi-address: %w", err)
+				}
+
+				log.Debugw("connecting to storage provider",
+					"id", addrInfo.ID, "multiaddrs", addrInfo.Addrs, "addr", maddr)
+
+				if err := n.Host.Connect(ctx, *addrInfo); err != nil {
+					return fmt.Errorf("connecting to peer %s: %w", addrInfo.ID, err)
+				}
+
+				protos, err := n.Host.Peerstore().GetProtocols(addrInfo.ID)
+				if err != nil {
+					return fmt.Errorf("getting protocols for peer %s: %w", addrInfo.ID, err)
+				}
+				sort.Strings(protos)
+
+				fmt.Print("Provider " + maddr.String())
+				if contains(protos, "/fil/storage/mk/1.2.0") {
+					fmt.Println(" running boost")
+				} else if contains(protos, "/fil/storage/mk/1.1.0") {
+					fmt.Println(" running markets")
+				}
+
+				//agentVersionI, err := n.Host.Peerstore().Get(addrInfo.ID, "AgentVersion")
+				//if err != nil {
+				//return fmt.Errorf("getting agent version for peer %s: %w", addrInfo.ID, err)
+				//}
+				//agentVersion, _ := agentVersionI.(string)
+
+				//if cctx.Bool("json") {
+				//return cmd.PrintJson(map[string]interface{}{
+				//"provider":   maddr.String(),
+				//"agent":      agentVersion,
+				//"id":         addrInfo.ID.String(),
+				//"multiaddrs": addrInfo.Addrs,
+				//"protocols":  protos,
+				//})
+				//}
+
+				//fmt.Println("Agent: " + agentVersion)
+				//fmt.Println("Peer ID: " + addrInfo.ID.String())
+				//fmt.Println("Peer Addresses:")
+				//for _, addr := range addrInfo.Addrs {
+				//fmt.Println("  " + addr.String())
+				//}
+				//fmt.Println("Protocols:\n" + "  " + strings.Join(protos, "\n  "))
+				return nil
+			}()
+			if err != nil {
+				fmt.Println("err: ", err)
+				continue
+			}
+		}
+
+		return nil
 	},
 }
 
@@ -294,4 +421,14 @@ var retrievalAskCmd = &cli.Command{
 
 		return nil
 	},
+}
+
+func contains(sl []string, substr string) bool {
+	for _, s := range sl {
+		if strings.Contains(s, substr) {
+			return true
+		}
+	}
+
+	return false
 }
