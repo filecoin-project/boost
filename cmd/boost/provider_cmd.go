3e7e17882e32d4033f588140813119dc9fdcce13
Anton Evangelatov <anton.evangelatov@gmail.com>
2022-06-29 14:08:23 +0200

    move cmd to boostx

--- cmd/boost/provider_cmd.go
+++ cmd/boost/provider_cmd.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"sort"
 	"strings"
-	"sync"
 
 	bcli "github.com/filecoin-project/boost/cli"
 	"github.com/filecoin-project/boost/cli/ctxutil"
@@ -29,124 +28,9 @@ var providerCmd = &cli.Command{
 	Usage: "Info about Storage Providers",
 	Flags: []cli.Flag{cmd.FlagRepo},
 	Subcommands: []*cli.Command{
-		runningBoostCmd,
 		libp2pInfoCmd,
 		storageAskCmd,
 		retrievalAskCmd,
-	},
-}
-
-var runningBoostCmd = &cli.Command{
-	Name:        "running-boost",
-	Usage:       "",
-	ArgsUsage:   "",
-	Description: "",
-	Before:      before,
-	Action: func(cctx *cli.Context) error {
-		ctx := ctxutil.ReqContext(cctx)
-
-		n, err := clinode.Setup(cctx.String(cmd.FlagRepo.Name))
-		if err != nil {
-			return fmt.Errorf("setting up CLI node: %w", err)
-		}
-
-		api, closer, err := lcli.GetGatewayAPI(cctx)
-		if err != nil {
-			return fmt.Errorf("setting up gateway connection: %w", err)
-		}
-		defer closer()
-
-		miners, err := api.StateListMiners(ctx, types.EmptyTSK)
-		if err != nil {
-			return err
-		}
-
-		fmt.Println("got len total miners: ", len(miners))
-
-		var wg sync.WaitGroup
-		wg.Add(len(miners))
-		var lk sync.Mutex
-		var withMinPower []address.Address
-
-		throttle := make(chan struct{}, 50)
-		for _, miner := range miners {
-			throttle <- struct{}{}
-			go func(miner address.Address) {
-				defer wg.Done()
-				defer func() {
-					<-throttle
-				}()
-
-				power, err := api.StateMinerPower(ctx, miner, types.EmptyTSK)
-				if err != nil {
-					panic(err)
-				}
-
-				if power.HasMinPower { // TODO: Lower threshold
-					lk.Lock()
-					withMinPower = append(withMinPower, miner)
-					lk.Unlock()
-				}
-			}(miner)
-		}
-
-		wg.Wait()
-
-		fmt.Println("got len miners with min power: ", len(withMinPower))
-
-		var boostNodes, marketsNodes, noProtocolsNodes int
-
-		for _, maddr := range withMinPower {
-			select {
-			case <-ctx.Done():
-				return nil
-			default:
-			}
-			err := func() error {
-				addrInfo, err := cmd.GetAddrInfo(ctx, api, maddr)
-				if err != nil {
-					return fmt.Errorf("getting provider multi-address: %w", err)
-				}
-
-				log.Debugw("connecting to storage provider",
-					"id", addrInfo.ID, "multiaddrs", addrInfo.Addrs, "addr", maddr)
-
-				if err := n.Host.Connect(ctx, *addrInfo); err != nil {
-					return fmt.Errorf("connecting to peer %s: %w", addrInfo.ID, err)
-				}
-
-				protos, err := n.Host.Peerstore().GetProtocols(addrInfo.ID)
-				if err != nil {
-					return fmt.Errorf("getting protocols for peer %s: %w", addrInfo.ID, err)
-				}
-				sort.Strings(protos)
-
-				fmt.Print("provider " + maddr.String())
-				if contains(protos, "/fil/storage/mk/1.2.0") {
-					fmt.Print(" running boost")
-					boostNodes++
-				} else if contains(protos, "/fil/storage/mk/1.1.0") {
-					fmt.Print(" running markets")
-					marketsNodes++
-				} else {
-					fmt.Print(" running fewer protocols")
-					noProtocolsNodes++
-				}
-				fmt.Println()
-
-				return nil
-			}()
-			if err != nil {
-				fmt.Println("err: ", err)
-				continue
-			}
-		}
-
-		fmt.Println("total boost nodes:", boostNodes)
-		fmt.Println("total markets nodes:", marketsNodes)
-		fmt.Println("total few-protocols nodes:", noProtocolsNodes)
-
-		return nil
 	},
 }
 
--- cmd/boostx/main.go
+++ cmd/boostx/main.go
@@ -33,6 +33,7 @@ func main() {
 			generatecarCmd,
 			marketAddCmd,
 			marketWithdrawCmd,
+			statsCmd,
 		},
 	}
 	app.Setup()
