753c5c399a9a7215e68e5b12cc4df91375ca5ec2
Anton Evangelatov <anton.evangelatov@gmail.com>
2022-08-03 12:44:53 +0300

    add cache=shared

--- node/modules/storageminer.go
+++ node/modules/storageminer.go
@@ -285,7 +285,7 @@ func StorageNetworkName(ctx helpers.MetricsCtx, a v1api.FullNode) (dtypes.Networ
 }
 
 func NewBoostDB(r lotus_repo.LockedRepo) (*sql.DB, error) {
-	dbPath := path.Join(r.Path(), "boost.db")
+	dbPath := path.Join(r.Path(), "boost.db?cache=shared")
 	return db.SqlDB(dbPath)
 }
 
@@ -294,7 +294,7 @@ type LogSqlDB struct {
 }
 
 func NewLogsSqlDB(r repo.LockedRepo) (*LogSqlDB, error) {
-	dbPath := path.Join(r.Path(), "boost.logs.db")
+	dbPath := path.Join(r.Path(), "boost.logs.db?cache=shared")
 	d, err := db.SqlDB(dbPath)
 	if err != nil {
 		return nil, err
