be41637745317d028a6932b020461b4c5eb25554
Anton Evangelatov <anton.evangelatov@gmail.com>
2022-09-05 14:50:14 +0200

    add booster-http image

--- docker/devnet/booster-http/entrypoint.sh
+++ docker/devnet/booster-http/entrypoint.sh
@@ -5,5 +5,9 @@ export FULLNODE_API_INFO=`lotus auth api-info --perm=admin | cut -f2 -d=`
 export MINER_API_INFO=`lotus-miner auth api-info --perm=admin | cut -f2 -d=`
 export BOOST_API_INFO=`boostd auth api-info --perm=admin | cut -f2 -d=`
 
+echo $FULLNODE_API_INFO
+echo $MINER_API_INFO
+echo $BOOST_API_INFO
+
 echo Starting booster-http...
 exec booster-http run --api-boost=$BOOST_API_INFO --api-fullnode=$FULLNODE_API_INFO --api-storage=$MINER_API_INFO
