ee72e7daf16adc0acfa79578b6acffefb342dde1
Anton Evangelatov <anton.evangelatov@gmail.com>
2022-10-04 16:44:28 +0200

    bump lotus docker images to v1.17.2-rc2 (#868)
    * bump lotus docker images to v1.17.2-rc2
    
    * update entrypoint.sh for booster-bitswap to do init on first run
    
    * add random.sh helper script

--- docker/devnet/booster-bitswap/entrypoint.sh
+++ docker/devnet/booster-bitswap/entrypoint.sh
@@ -8,6 +8,16 @@ export BOOST_API_INFO=`boostd auth api-info --perm=admin | cut -f2 -d=`
 echo $FULLNODE_API_INFO
 echo $MINER_API_INFO
 echo $BOOST_API_INFO
+echo $BOOSTER_BITSWAP_REPO
+
+if [ ! -f $BOOSTER_BITSWAP_REPO/.init.booster-bitswap ]; then
+	echo Init booster-bitswap on first run ...
+
+	booster-bitswap init
+
+	echo Done
+	touch $BOOSTER_BITSWAP_REPO/.init.booster-bitswap
+fi
 
 echo Starting booster-bitswap...
 exec booster-bitswap run --api-boost=$BOOST_API_INFO --tracing
