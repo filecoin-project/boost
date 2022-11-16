72ac0a63d08fea6922ecfc650cd141aa5dae8e52
Dirk McCormick <dirkmdev@gmail.com>
2022-11-16 11:47:02 +0100

    feat: retrieval logs

--- react/src/App.js
+++ react/src/App.js
@@ -21,7 +21,10 @@ import {PieceMetaDetail, PieceMetaPage} from "./PieceMeta";
 import {ProposalLogsPage} from "./ProposalLogs";
 import {InspectPage} from "./Inspect";
 import {RetrievalLogsPage} from "./RetrievalLogs";
+<<<<<<< HEAD
 import {RetrievalLogDetail} from "./RetrievalLogDetail";
+=======
+>>>>>>> feat: retrieval logs
 
 function App(props) {
     return (
@@ -44,7 +47,10 @@ function App(props) {
                                         <Route path="/proposal-logs/from/:cursor/page/:pageNum" element={<ProposalLogsPage />} />
                                         <Route path="/retrieval-logs" element={<RetrievalLogsPage />} />
                                         <Route path="/retrieval-logs/from/:cursor/page/:pageNum" element={<RetrievalLogsPage />} />
+<<<<<<< HEAD
                                         <Route path="/retrieval-logs/:peerID/:dealID" element={<RetrievalLogDetail />} />
+=======
+>>>>>>> feat: retrieval logs
                                         <Route path="/storage-space" element={<StorageSpacePage />} />
                                         <Route path="/sealing-pipeline" element={<SealingPipelinePage />} />
                                         <Route path="/funds" element={<FundsPage />} />
