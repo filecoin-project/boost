6dea7a69148e7c612c57d00dd6f48dc69cf43a38
LexLuthr <lexluthr@protocol.ai>
2022-11-23 01:58:53 +0530

    migrate config to v4

---
+++ node/config/v3_to_v4.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"fmt"
+)
+
+// Migrate from config version 3 to version 4
+func v3Tov4(cfgPath string) (string, error) {
+	cfg, err := FromFile(cfgPath, DefaultBoost())
+	if err != nil {
+		return "", fmt.Errorf("parsing config file %s: %w", cfgPath, err)
+	}
+
+	boostCfg, ok := cfg.(*Boost)
+	if !ok {
+		return "", fmt.Errorf("unexpected config type %T: expected *config.Boost", cfg)
+	}
+
+	// Update the Boost config version
+	boostCfg.ConfigVersion = 4
+
+	bz, err := ConfigUpdate(boostCfg, DefaultBoost(), true)
+	if err != nil {
+		return "", fmt.Errorf("applying configuration: %w", err)
+	}
+
+	return string(bz), nil
+}
