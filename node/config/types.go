9fa6415f87455761274a042a2d104d4f9129ed3e
LexLuthr <lexluthr@protocol.ai>
2022-11-22 16:08:23 +0530

    fix deal filter description

--- node/config/types.go
+++ node/config/types.go
@@ -118,10 +118,10 @@ type LotusDealmakingConfig struct {
 	StartEpochSealingBuffer uint64
 
 	// A command used for fine-grained evaluation of storage deals
-	// see https://docs.filecoin.io/mine/lotus/miner-configuration/#using-filters-for-fine-grained-storage-and-retrieval-deal-acceptance for more details
+	// see https://boost.filecoin.io/configuration/deal-filters for more details
 	Filter string
 	// A command used for fine-grained evaluation of retrieval deals
-	// see https://docs.filecoin.io/mine/lotus/miner-configuration/#using-filters-for-fine-grained-storage-and-retrieval-deal-acceptance for more details
+	// see https://boost.filecoin.io/configuration/deal-filters for more details
 	RetrievalFilter string
 
 	RetrievalPricing *lotus_config.RetrievalPricing
@@ -176,10 +176,10 @@ type DealmakingConfig struct {
 	DealProposalLogDuration Duration
 
 	// A command used for fine-grained evaluation of storage deals
-	// see https://docs.filecoin.io/mine/lotus/miner-configuration/#using-filters-for-fine-grained-storage-and-retrieval-deal-acceptance for more details
+	// see https://boost.filecoin.io/configuration/deal-filters for more details
 	Filter string
 	// A command used for fine-grained evaluation of retrieval deals
-	// see https://docs.filecoin.io/mine/lotus/miner-configuration/#using-filters-for-fine-grained-storage-and-retrieval-deal-acceptance for more details
+	// see https://boost.filecoin.io/configuration/deal-filters for more details
 	RetrievalFilter string
 
 	RetrievalPricing *lotus_config.RetrievalPricing
