e9d6ea27fd3f8e483f3546d48585afbefee7fceb
Anton Evangelatov <anton.evangelatov@gmail.com>
2022-12-01 17:03:23 +0100

    fixup

--- react/src/Menu.js
+++ react/src/Menu.js
@@ -9,6 +9,7 @@ import gridImg from './bootstrap-icons/icons/grid-3x3-gap.svg'
 import './Menu.css'
 import {SettingsMenuItem} from "./Settings";
 import {InspectMenuItem} from "./Inspect";
+import {PiecesMenuItem} from "./PieceDirectory";
 import {ProposalLogsMenuItem} from "./ProposalLogs";
 import {RetrievalLogsMenuItem} from "./RetrievalLogs";
 
