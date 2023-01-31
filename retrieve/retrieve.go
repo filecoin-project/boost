e23fdc9e50f02e657940dd558b4492c409dcbeb7
Anton Evangelatov <anton.evangelatov@gmail.com>
2023-01-31 15:00:53 +0100

    remove unused method

--- retrieve/retrieve.go
+++ retrieve/retrieve.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -1031,38 +1030,6 @@ func (fc *Client) OnRetrievalEvent(event rep.RetrievalEvent) {
 		logadd("receivedSize", tevent.ReceivedSize())
 	}
 	log.Debugw("retrieval-event", kv...)
-}
-
-func ChannelIDFromString(id string) (*datatransfer.ChannelID, error) {
-	if id == "" {
-		return nil, fmt.Errorf("cannot parse empty string as channel id")
-	}
-
-	parts := strings.Split(id, "-")
-	if len(parts) != 3 {
-		return nil, fmt.Errorf("cannot parse channel id '%s': expected format 'initiator-responder-transferid'", id)
-	}
-
-	initiator, err := peer.Decode(parts[0])
-	if err != nil {
-		return nil, fmt.Errorf("parsing initiator peer id '%s' in channel id '%s'", parts[0], id)
-	}
-
-	responder, err := peer.Decode(parts[1])
-	if err != nil {
-		return nil, fmt.Errorf("parsing responder peer id '%s' in channel id '%s'", parts[1], id)
-	}
-
-	xferid, err := strconv.ParseUint(parts[2], 10, 64)
-	if err != nil {
-		return nil, fmt.Errorf("parsing transfer id '%s' in channel id '%s'", parts[2], id)
-	}
-
-	return &datatransfer.ChannelID{
-		Initiator: initiator,
-		Responder: responder,
-		ID:        datatransfer.TransferID(xferid),
-	}, nil
 }
 
 var dealIdGen = shared.NewTimeCounter()
