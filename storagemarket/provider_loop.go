8c9381ebd32c5f10e13041923018275fe8d31a5b
Anton Evangelatov <anton.evangelatov@gmail.com>
2023-02-22 12:58:43 +0100

    check for deal uniqueness

--- storagemarket/provider_loop.go
+++ storagemarket/provider_loop.go
@@ -283,8 +283,6 @@ func (p *Provider) processImportOfflineDealData(deal *types.ProviderDealState) *
 }
 
 func (p *Provider) checkDealPropUnique(deal *smtypes.ProviderDealState) *acceptError {
-	return nil
-
 	signedPropCid, err := deal.SignedProposalCid()
 	if err != nil {
 		return &acceptError{
