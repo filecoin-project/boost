f201a6753de079ce44ed2753bb2cdac0dd40073a
Dirk McCormick <dirkmdev@gmail.com>
2023-04-12 14:05:50 +0200

    feat: group agent version by binary name

---
+++ cmd/boostx/stats_cmd_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestAgentVersion(t *testing.T) {
+	testCases := map[string]string{
+		"lotus-1.20.0+gzbt+git.cddeab21d.dirty": "lotus-1.20.0+gzbt",
+		"boost-1.6.1+git.4931e18":               "boost-1.6.1",
+		"boost-1.6.2-rc1+git.0b5cf47.dirty":     "boost-1.6.2-rc1",
+		"lotus-1.20.0":                          "lotus-1.20.0",
+	}
+	for agentVersion, exp := range testCases {
+		agentVersionShort := shortAgentVersion(agentVersion)
+		require.Equal(t, exp, agentVersionShort)
+	}
+}
