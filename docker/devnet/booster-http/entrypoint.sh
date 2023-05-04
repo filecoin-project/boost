45d6677118ec2351c2b39f2c8649ce17b8cc0d64
Jacob Heun <jacobheun@gmail.com>
2023-05-04 06:05:06 -0700

    chore: enable files gateway in devnet by default (#1420)

--- docker/devnet/booster-http/entrypoint.sh
+++ docker/devnet/booster-http/entrypoint.sh
@@ -10,4 +10,4 @@ echo $MINER_API_INFO
 echo $BOOST_API_INFO
 
 echo Starting booster-http...
-exec booster-http run --api-boost=$BOOST_API_INFO --api-fullnode=$FULLNODE_API_INFO --api-storage=$MINER_API_INFO --tracing
+exec booster-http run --serve-files=true --api-boost=$BOOST_API_INFO --api-fullnode=$FULLNODE_API_INFO --api-storage=$MINER_API_INFO --tracing
