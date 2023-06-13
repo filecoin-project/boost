978fe850a4761f3161c128f43cb25f84be14b481
LexLuthr <lexluthr@protocol.ai>
2023-06-13 18:32:35 +0530

    fix js

--- react/src/MonitoringAlert.js
+++ react/src/MonitoringAlert.js
@@ -3,18 +3,19 @@ import './MonitoringAlert.css'
 import warnImg from "./bootstrap-icons/icons/exclamation-circle.svg"
 import {Link} from "react-router-dom";
 import {useQuery} from "@apollo/react-hooks";
-import {MpoolAlertQuery} from "./gql";
+import {MpoolAlertsQuery} from "./gql";
 
 export function MonitoringAlert(props) {
-    const data = useQuery(MpoolAlertQuery, {
-        pollInterval: 10000,
+    const data = useQuery(MpoolAlertsQuery, {
+        pollInterval: 100,
         fetchPolicy: `network-only`,
     })
 
     var count = 0
-    if (data > 0) {
-        count = data
-    } else {
+    if (data) {
+        count = data.mpoolAlertCount
+    }
+    if (!count) {
         return null
     }
 
