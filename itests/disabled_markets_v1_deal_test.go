72466b7f248125bd59fde9ebb5ff3a8c537c192e
LexLuthr <lexluthr@protocol.ai>
2023-06-19 18:36:45 +0530

    enable itest in CI

--- itests/disabled_markets_v1_deal_test.go
+++ itests/disabled_markets_v1_deal_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestMarketsV1FailDeal(t *testing.T) {
+func TestDisabledMarketsV1Deal(t *testing.T) {
 	ctx := context.Background()
 	log := framework.Log
 
