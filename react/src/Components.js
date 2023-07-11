1828ac0893a7d1350f931857b216f1c97dc31592
Dirk McCormick <dirkmdev@gmail.com>
2023-07-11 11:56:48 +0200

    feat: IPNI UX

--- react/src/Components.js
+++ react/src/Components.js
@@ -1,5 +1,7 @@
-import React from "react";
+/* global BigInt */
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
+import {addCommas} from "./util";
 
 export function PageContainer(props) {
     return (
@@ -35,4 +37,46 @@ export function ShortPeerID(props) {
 export function ShortCID(props) {
     const cid = props.cid.substring(0, 8) + '…'
     return <div>{cid}</div>
+}
+
+export function ShortCIDTail(props) {
+    const cid = '…' + props.cid.slice(-8)
+    return <div>{cid}</div>
+}
+
+export function ExpandableJSObject(props) {
+    const [expanded, setExpanded] = useState(props.expanded || false)
+
+    var val = props.v
+    const isObject = (val && typeof val === 'object')
+    if (isObject) {
+        const isArray = Array.isArray(val)
+        val = Object.keys(val).sort().map(ck => <ExpandableJSObject k={ck} v={val[ck]} key={ck} arrEl={isArray} />)
+    } else if ((typeof val === 'string' || typeof val === 'number') && (''+val).match(/^[0-9]+$/)) {
+        val = addCommas(BigInt(val))
+    } else if (typeof val == "boolean") {
+        val = val + ''
+    }
+
+    function toggleExpandState() {
+        setExpanded(!expanded)
+    }
+
+    const expandable = isObject && props.topLevel
+    return (
+        <div className={"js-obj" + (expandable ? ' expandable' : '') + (expanded ? ' expanded' : '')}>
+            {props.k && !props.arrEl ? (
+                <>
+                <span className="js-obj-name" onClick={toggleExpandState}>
+                    {props.k}:
+                    {expandable ? (
+                        <div className="expand-collapse"></div>
+                    ) : null}
+                </span>
+                &nbsp;
+                </>
+            ) : null}
+            <span className="js-obj-val">{val}</span>
+        </div>
+    )
 }
