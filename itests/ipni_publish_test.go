d715f70659e08aacb2ca37a062b0937f29e81271
LexLuthr <88259624+LexLuthr@users.noreply.github.com>
2023-07-12 18:03:36 +0400

    feat: add IPNI itest (#1563)
    * ipni itest
    
    * refactor test
    
    * add to circleCI
    
    * add indexer topic
    
    * Print protocol IDs exposed by f.Boost
    
    * generate topic name dynamically
    
    ---------
    
    Co-authored-by: Masih H. Derkani <m@derkani.org>

---
+++ itests/ipni_publish_test.go
@@ -0,0 +1,103 @@
+package itests
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	boost_build "github.com/filecoin-project/boost/build"
+	"github.com/filecoin-project/boost/itests/framework"
+	"github.com/filecoin-project/boost/node/modules/dtypes"
+	"github.com/filecoin-project/boost/testutil"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/itests/kit"
+	"github.com/google/uuid"
+	"github.com/ipfs/go-cid"
+	"github.com/ipni/ipni-cli/pkg/adpub"
+	"github.com/stretchr/testify/require"
+)
+
+func TestIPNIPublish(t *testing.T) {
+	ctx := context.Background()
+	log := framework.Log
+
+	kit.QuietMiningLogs()
+	framework.SetLogLevel()
+	var opts []framework.FrameworkOpts
+	opts = append(opts, framework.EnableLegacyDeals(true))
+	f := framework.NewTestFramework(ctx, t, opts...)
+	err := f.Start()
+	require.NoError(t, err)
+	defer f.Stop()
+
+	// Get the listen address
+	addrs, err := f.Boost.NetAddrsListen(ctx)
+	require.NoError(t, err)
+
+	ntwkName, err := f.FullNode.StateNetworkName(ctx)
+	require.NoError(t, err)
+
+	topicName := boost_build.IndexerIngestTopic(dtypes.NetworkName(ntwkName))
+
+	// Create new ipni-cli client
+	ipniClient, err := adpub.NewClient(addrs, adpub.WithTopicName(topicName), adpub.WithEntriesDepthLimit(100000))
+	require.NoError(t, err)
+
+	// Get head when boost starts
+	headAtStart, err := ipniClient.GetAdvertisement(ctx, cid.Undef)
+	require.NoError(t, err)
+
+	err = f.AddClientProviderBalance(abi.NewTokenAmount(1e15))
+	require.NoError(t, err)
+
+	// Create a CAR file
+	tempdir := t.TempDir()
+	log.Debugw("using tempdir", "dir", tempdir)
+
+	fileSize := 200000
+	randomFilepath, err := testutil.CreateRandomFile(tempdir, 5, fileSize)
+	require.NoError(t, err)
+
+	// create a dense carv2 for deal making
+	rootCid, carFilepath, err := testutil.CreateDenseCARv2(tempdir, randomFilepath)
+	require.NoError(t, err)
+
+	// Start a web server to serve the car files
+	log.Debug("starting webserver")
+	server, err := testutil.HttpTestFileServer(t, tempdir)
+	require.NoError(t, err)
+	defer server.Close()
+
+	// Create a new dummy deal
+	log.Debug("creating dummy deal")
+	dealUuid := uuid.New()
+
+	// Make a deal
+	res, err := f.MakeDummyDeal(dealUuid, carFilepath, rootCid, server.URL+"/"+filepath.Base(carFilepath), false)
+	require.NoError(t, err)
+	require.True(t, res.Result.Accepted)
+	log.Debugw("got response from MarketDummyDeal", "res", spew.Sdump(res))
+
+	// Wait for the deal to be added to a sector
+	err = f.WaitForDealAddedToSector(dealUuid)
+	require.NoError(t, err)
+
+	// Get current head after the deal
+	headAfterDeal, err := ipniClient.GetAdvertisement(ctx, cid.Undef)
+	require.NoError(t, err)
+
+	// Verify new advertisement is added to the chain and previous head is linked
+	require.Equal(t, headAtStart.ID, headAfterDeal.PreviousID)
+
+	// Confirm this ad is not a remove type
+	require.False(t, headAfterDeal.IsRemove)
+
+	// Get all multihashes - indexer retrieval
+	mhs, err := headAfterDeal.Entries.Drain()
+	require.NoError(t, err)
+
+	require.Greater(t, len(mhs), 0)
+
+	require.Contains(t, mhs, rootCid.Hash())
+}
