ed5d2c2effa9a8a7d17a8e853ef62f6d64791c77
Anton Evangelatov <anton.evangelatov@gmail.com>
2023-08-15 17:00:25 +0200

    steps to onboard direct data

--- storagemarket/direct_deals_provider.go
+++ storagemarket/direct_deals_provider.go
@@ -28,5 +28,25 @@ func (ddp *DirectDealsProvider) Import(ctx context.Context, piececid cid.Cid, fi
 	}
 	defer fi.Close() //nolint:errcheck
 
+	////////////////////////////////////////////////////
+	// 1. Validate the deal proposal
+	//if err := p.validateDealProposal(ds); err != nil {
+
+	////////////////////////////////////////////////////
+	// 2. Check for deal acceptance
+	//resp, err := p.checkForDealAcceptance(ctx, &ds, false)
+
+	////////////////////////////////////////////////////
+	// 3. Process direct deal proposal
+	//aerr := p.processOfflineDealProposal(dealReq.deal, dh)
+
+	////////////////////////////////////////////////////
+	// 4. Process direct deal filepath data
+	//aerr := p.processImportOfflineDealData(dealReq.deal, dh)
+
+	////////////////////////////////////////////////////
+	// 5. verify CommP matches for an offline deal
+	//if err := p.verifyCommP(deal); err != nil {
+
 	return nil, nil
 }
