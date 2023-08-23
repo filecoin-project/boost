2d54098f08232f21f6f7b7725947a60e66e228d0
LexLuthr <lexluthr@protocol.ai>
2023-08-23 15:13:56 +0400

    fix boostd-data maddr, enhance build

--- extern/boostd-data/build/build.go
+++ extern/boostd-data/build/build.go
@@ -1,9 +1,50 @@
 package build
 
+import (
+	"github.com/filecoin-project/go-address"
+)
+
 var CurrentCommit string
+
+func SetAddressNetwork(n address.Network) {
+	address.CurrentNetwork = n
+}
+
+var BuildType int
+
+const (
+	BuildDefault      = 0
+	BuildMainnet      = 0x1
+	Build2k           = 0x2
+	BuildDebug        = 0x3
+	BuildCalibnet     = 0x4
+	BuildInteropnet   = 0x5
+	BuildButterflynet = 0x7
+)
+
+func BuildTypeString() string {
+	switch BuildType {
+	case BuildDefault:
+		return ""
+	case BuildMainnet:
+		return "+mainnet"
+	case Build2k:
+		return "+2k"
+	case BuildDebug:
+		return "+debug"
+	case BuildCalibnet:
+		return "+calibnet"
+	case BuildInteropnet:
+		return "+interopnet"
+	case BuildButterflynet:
+		return "+butterflynet"
+	default:
+		return "+huh?"
+	}
+}
 
 const BuildVersion = "1.4.0"
 
 func UserVersion() string {
-	return BuildVersion + CurrentCommit
+	return BuildVersion + BuildTypeString() + CurrentCommit
 }
