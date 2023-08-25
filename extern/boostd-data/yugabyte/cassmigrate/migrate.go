0d235b92bd2ea6ffc02c7689779c01f7f466db73
Dirk McCormick <dirkmdev@gmail.com>
2023-08-25 13:59:50 +0200

    refactor: dbname -> appliedMigration

--- extern/boostd-data/yugabyte/cassmigrate/migrate.go
+++ extern/boostd-data/yugabyte/cassmigrate/migrate.go
@@ -41,10 +41,10 @@ func Migrate(ctx context.Context, session *gocql.Session) error {
 	// Get a list of migrations that have already completed
 	qry = `SELECT Name from migration_version`
 	iter := session.Query(qry).WithContext(ctx).Iter()
-	var dbnames []string
-	var name string
-	for iter.Scan(&name) {
-		dbnames = append(dbnames, name)
+	var appliedMigrations []string
+	var appliedMigration string
+	for iter.Scan(&appliedMigration) {
+		appliedMigrations = append(appliedMigrations, appliedMigration)
 	}
 	if err := iter.Close(); err != nil {
 		return fmt.Errorf("getting migrations from migration_version: %w", err)
@@ -60,11 +60,11 @@ func Migrate(ctx context.Context, session *gocql.Session) error {
 		return nil
 	}
 
-	return executeMigrations(ctx, session, migrations, dbnames, writeComplete)
+	return executeMigrations(ctx, session, migrations, appliedMigrations, writeComplete)
 }
 
-func executeMigrations(ctx context.Context, session *gocql.Session, migs []migration, dbnames []string, writeComplete func(string) error) error {
-	sort.Strings(dbnames)
+func executeMigrations(ctx context.Context, session *gocql.Session, migs []migration, appliedMigrations []string, writeComplete func(string) error) error {
+	sort.Strings(appliedMigrations)
 	sort.Slice(migs, func(i, j int) bool {
 		return migs[i].name < migs[j].name
 	})
@@ -72,17 +72,17 @@ func executeMigrations(ctx context.Context, session *gocql.Session, migs []migra
 	// For each migration
 	for i, m := range migs {
 		// Skip completed migrations
-		if i < len(dbnames) {
+		if i < len(appliedMigrations) {
 			// Check the migrations order matches between the db and the executable
-			if m.name != dbnames[i] {
-				err := fmt.Errorf("migrations order mismatch with db: at position %d expected %s but got %s", i, dbnames[i], m.name)
+			if m.name != appliedMigrations[i] {
+				err := fmt.Errorf("migrations order mismatch with db: at position %d expected %s but got %s", i, appliedMigrations[i], m.name)
 				l := err.Error() + "\n"
-				l += fmt.Sprintf("migrations to run: %d\n", len(dbnames))
+				l += fmt.Sprintf("migrations to run: %d\n", len(appliedMigrations))
 				for mi, mig := range migs {
 					l += fmt.Sprintf("  %d.\t%s\n", mi, mig.name)
 				}
-				l += fmt.Sprintf("completed migrations in db: %d\n", len(dbnames))
-				for mi, dbname := range dbnames {
+				l += fmt.Sprintf("completed migrations in db: %d\n", len(appliedMigrations))
+				for mi, dbname := range appliedMigrations {
 					l += fmt.Sprintf("  %d.\t%s\n", mi, dbname)
 				}
 				log.Error(l)
