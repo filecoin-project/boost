b695e55d6f8633907fe856e19caa085840020f0c
Dirk McCormick <dirkmdev@gmail.com>
2023-09-08 15:44:13 +0200

    refactor: move datacap code from ddo test function into itest framework

--- itests/ddo_test.go
+++ itests/ddo_test.go
@@ -9,105 +9,38 @@ import (
 	smtypes "github.com/filecoin-project/boost/storagemarket/types"
 	"github.com/filecoin-project/boost/testutil"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"
 	verifregst "github.com/filecoin-project/go-state-types/builtin/v9/verifreg"
-	verifregtypes "github.com/filecoin-project/go-state-types/builtin/v9/verifreg"
-	lapi "github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/verifreg"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/chain/wallet/key"
 	"github.com/filecoin-project/lotus/itests/kit"
 	"github.com/google/uuid"
-	logging "github.com/ipfs/go-log/v2"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestDirectDeal(t *testing.T) {
 	ctx := context.Background()
+	fileSize := 2000000
+	pieceSize := 8388608
 
 	kit.QuietMiningLogs()
 	framework.SetLogLevel()
-	_ = logging.SetLogLevel("fxlog", "WARN")
 
-	rootKey, err := key.GenerateKey(types.KTSecp256k1)
-	require.NoError(t, err)
-
-	verifierKey, err := key.GenerateKey(types.KTSecp256k1)
-	require.NoError(t, err)
-
-	bal, err := types.ParseFIL("100fil")
+	// Build parameters needed to set up datacap
+	dataCapParams, err := framework.BuildDatacapParams()
 	require.NoError(t, err)
 
 	var opts []framework.FrameworkOpts
 	opts = append(opts, framework.EnableLegacyDeals(true))
-	opts = append(opts, framework.EnsembleOpts(
-		kit.RootVerifier(rootKey, abi.NewTokenAmount(bal.Int64())),
-		// assign some balance to the verifier so they can send an AddClient message.
-		kit.Account(verifierKey, abi.NewTokenAmount(bal.Int64())),
-	))
+	opts = append(opts, framework.EnsembleOpts(dataCapParams.Opts...))
 	f := framework.NewTestFramework(ctx, t, opts...)
 	err = f.Start()
 	require.NoError(t, err)
 	defer f.Stop()
 
-	vrh, err := f.FullNode.StateVerifiedRegistryRootKey(ctx, types.TipSetKey{})
+	// Give the boost client's address enough datacap to make the deal
+	err = f.AddClientDataCap(ctx, dataCapParams.RootKey, dataCapParams.VerifierKey, pieceSize)
 	require.NoError(t, err)
-	t.Log("Verified Registry Root Key", vrh.String())
-
-	// import the root key.
-	rootAddr, err := f.FullNode.WalletImport(ctx, &rootKey.KeyInfo)
-	require.NoError(t, err)
-
-	// import the verifier's key.
-	verifierAddr, err := f.FullNode.WalletImport(ctx, &verifierKey.KeyInfo)
-	require.NoError(t, err)
-
-	params, err := actors.SerializeParams(&verifregtypes.AddVerifierParams{Address: verifierAddr, Allowance: big.NewInt(100000000000)})
-	require.NoError(t, err)
-
-	msg := &types.Message{
-		From:   rootAddr,
-		To:     verifreg.Address,
-		Method: verifreg.Methods.AddVerifier,
-		Params: params,
-		Value:  big.Zero(),
-	}
-
-	sm, err := f.FullNode.MpoolPushMessage(ctx, msg, nil)
-	require.NoError(t, err, "AddVerifier failed")
-	res, err := f.FullNode.StateWaitMsg(ctx, sm.Cid(), 1, lapi.LookbackNoLimit, true)
-	require.NoError(t, err)
-	require.EqualValues(t, 0, res.Receipt.ExitCode)
-
-	// assign datacap to client
-	fileSize := 2000000
-	pieceSize := int64(8388608)
-	datacap := big.NewInt(pieceSize)
-
-	params, err = actors.SerializeParams(&verifregtypes.AddVerifiedClientParams{Address: f.ClientAddr, Allowance: datacap})
-	require.NoError(t, err)
-
-	msg = &types.Message{
-		From:   verifierAddr,
-		To:     verifreg.Address,
-		Method: verifreg.Methods.AddVerifiedClient,
-		Params: params,
-		Value:  big.Zero(),
-	}
-
-	sm, err = f.FullNode.MpoolPushMessage(ctx, msg, nil)
-	require.NoError(t, err)
-
-	res, err = f.FullNode.StateWaitMsg(ctx, sm.Cid(), 1, lapi.LookbackNoLimit, true)
-	require.NoError(t, err)
-	require.EqualValues(t, 0, res.Receipt.ExitCode)
-
-	// check datacap balance
-	dcap, err := f.FullNode.StateVerifiedClientStatus(ctx, f.ClientAddr, types.EmptyTSK)
-	require.NoError(t, err)
-	require.Equal(t, *dcap, datacap)
 
 	err = f.AddClientProviderBalance(abi.NewTokenAmount(1e15))
 	require.NoError(t, err)
@@ -129,7 +62,7 @@ func TestDirectDeal(t *testing.T) {
 		verifregst.MaximumVerifiedAllocationExpiration)
 	require.NoError(t, err)
 
-	sm, err = f.FullNode.MpoolPushMessage(ctx, allocateMsg, nil)
+	sm, err := f.FullNode.MpoolPushMessage(ctx, allocateMsg, nil)
 	require.NoError(t, err)
 
 	_, err = f.FullNode.StateWaitMsg(ctx, sm.Cid(), 1, 1e10, true)
