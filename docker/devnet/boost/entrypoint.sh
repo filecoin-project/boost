f97e4f9883317726d840f5c575ad359165b7172c
Anton Evangelatov <anton.evangelatov@gmail.com>
2023-09-21 13:44:18 +0200

    fix config.toml for devnet (#1708)
    * fix config.toml for devnet
    
    * fixup
    
    * run boostd-data earlier

--- docker/devnet/boost/entrypoint.sh
+++ docker/devnet/boost/entrypoint.sh
@@ -46,6 +46,14 @@ if [ ! -f $BOOST_PATH/.init.boost ]; then
 	touch $BOOST_PATH/.init.boost
 fi
 
+## Override config options
+echo Updating config values
+sed 's|#ServiceApiInfo = ""|ServiceApiInfo = "ws://localhost:8042"|g' $BOOST_PATH/config.toml > $BOOST_PATH/config.toml.tmp; cp $BOOST_PATH/config.toml.tmp $BOOST_PATH/config.toml; rm $BOOST_PATH/config.toml.tmp
+sed 's|#ExpectedSealDuration = "24h0m0s"|ExpectedSealDuration = "0h0m10s"|g' $BOOST_PATH/config.toml > $BOOST_PATH/config.toml.tmp; cp $BOOST_PATH/config.toml.tmp $BOOST_PATH/config.toml; rm $BOOST_PATH/config.toml.tmp
+
+## run boostd-data
+boostd-data run leveldb --addr=0.0.0.0:8042 &
+
 # TODO(anteva): fixme: hack as boostd fails to start without this dir
 mkdir -p /var/lib/boost/deal-staging
 
@@ -74,11 +82,6 @@ if [ ! -f $BOOST_PATH/.register.boost ]; then
 	echo Super. DONE! Boostd is now configured and will be started soon
 fi
 
-## Override config options
-echo Updating config values
-sed 's|ServiceApiInfo = ""|ServiceApiInfo = "ws://localhost:8042"|g' $BOOST_PATH/config.toml > $BOOST_PATH/config.toml.tmp; cp $BOOST_PATH/config.toml.tmp $BOOST_PATH/config.toml; rm $BOOST_PATH/config.toml.tmp
-sed 's|ExpectedSealDuration = "24h0m0s"|ExpectedSealDuration = "0h0m10s"|g' $BOOST_PATH/config.toml > $BOOST_PATH/config.toml.tmp; cp $BOOST_PATH/config.toml.tmp $BOOST_PATH/config.toml; rm $BOOST_PATH/config.toml.tmp
-
 echo Starting LID service and boost in dev mode...
 trap 'kill %1' SIGINT
-exec boostd-data run leveldb --addr=0.0.0.0:8042 & boostd -vv run --nosync=true
+exec boostd -vv run --nosync=true
