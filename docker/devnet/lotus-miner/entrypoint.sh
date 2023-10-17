efb90960c5afe3bdaf46b9916520a417ae6e96c8
Anton Evangelatov <anton.evangelatov@gmail.com>
2023-10-17 15:56:57 +0200

    multi-miner devnet (#1758)

--- docker/devnet/lotus-miner/entrypoint.sh
+++ docker/devnet/lotus-miner/entrypoint.sh
@@ -5,9 +5,9 @@ lotus wait-api
 echo Lotus ready. Lets go
 if [ ! -f $LOTUS_MINER_PATH/.init.miner ]; then
 	echo Import the genesis miner key ...
-	lotus wallet import --as-default $GENESIS_PATH/pre-seal-t01000.key
+	lotus wallet import --as-default $GENESIS_PATH/pre-seal-$MINER_ADDR.key
 	echo Set up the genesis miner ...
-	lotus-miner init --genesis-miner --actor=t01000 --sector-size=$SECTOR_SIZE --pre-sealed-sectors=$GENESIS_PATH --pre-sealed-metadata=$GENESIS_PATH/pre-seal-t01000.json --nosync
+	lotus-miner init --genesis-miner --actor=$MINER_ADDR --sector-size=$SECTOR_SIZE --pre-sealed-sectors=$GENESIS_PATH --pre-sealed-metadata=$GENESIS_PATH/pre-seal-$MINER_ADDR.json --nosync
 	touch $LOTUS_MINER_PATH/.init.miner
 	echo Done
 fi
