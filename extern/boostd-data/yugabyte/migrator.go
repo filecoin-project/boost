9c60b171f4bdcb9b8ac0f400c848ba4351064f95
LexLuthr <lexluthr@protocol.ai>
2023-11-08 11:57:20 +0400

    fix additional keys

--- extern/boostd-data/yugabyte/migrator.go
+++ extern/boostd-data/yugabyte/migrator.go
@@ -92,9 +92,9 @@ func StripLoadBalance(connectString string) (string, error) {
 	}
 	q := u.Query()
 	q.Del("load_balance")
-	q.Del("statement_cache_capacity")
-	q.Del("statement_cache_mode")
-	q.Del("prefer_simple_protocol")
+	q.Del("yb_servers_refresh_interval")
+	q.Del("failed-host-reconnect-delay-secs")
+	q.Del("fallback-to-topology-keys-only")
 	q.Del("topology_keys")
 	u.RawQuery = q.Encode()
 	return u.String(), nil
