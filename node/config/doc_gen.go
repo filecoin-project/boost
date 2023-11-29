69030f68c54f54611ca683e6db985f8831e8e5c7
Ivan Schasny <31857042+ischasny@users.noreply.github.com>
2023-11-29 13:03:15 +0000

    fix: set default add index concurrency to 8 (#1829)
    * fix: set default add index concurrency to 8
    
    * Update docs

--- node/config/doc_gen.go
+++ node/config/doc_gen.go
@@ -643,7 +643,7 @@ is in the piece, and adds the index to the local index directory.`,
 			Type: "int",
 
 			Comment: `AddIndexConcurrency sets the number of concurrent tasks that each add index operation is split into.
-This setting is usefull to better utilise bandwidth between boostd and boost-data. The default value is 4.`,
+This setting is usefull to better utilise bandwidth between boostd and boost-data. The default value is 8.`,
 		},
 		{
 			Name: "EmbeddedServicePort",
