d271f454a7db3ba544d317def4ee71a1e204df6e
LexLuthr <lexluthr@curiostorage.org>
2024-07-29 15:15:04 +0400

    release v2.3.0

--- build/version.go
+++ build/version.go
@@ -31,7 +31,7 @@ func BuildTypeString() string {
 	}
 }
 
-const BuildVersion = "2.3.0-rc2"
+const BuildVersion = "2.3.0"
 
 func UserVersion() string {
 	return BuildVersion + BuildTypeString() + CurrentCommit
