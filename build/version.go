86de5091c9025aa7644a3295995b3c53107fe414
LexLuthr <88259624+LexLuthr@users.noreply.github.com>
2024-12-12 15:56:48 +0400

    release v2.4.1-rc1 (#1988)

--- build/version.go
+++ build/version.go
@@ -31,7 +31,7 @@ func BuildTypeString() string {
 	}
 }
 
-const BuildVersion = "2.4.0"
+const BuildVersion = "2.4.1-rc1"
 
 func UserVersion() string {
 	return BuildVersion + BuildTypeString() + CurrentCommit
