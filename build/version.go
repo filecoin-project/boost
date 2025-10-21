46ad4b4b3471b9fabd8d1c50345321812d0c1420
Andrew Jackson (Ajax) <snadrus@gmail.com>
2025-10-21 10:21:01 -0500

    bump version number

--- build/version.go
+++ build/version.go
@@ -31,7 +31,7 @@ func BuildTypeString() string {
 	}
 }
 
-const BuildVersion = "2.4.4"
+const BuildVersion = "2.4.5"
 
 func UserVersion() string {
 	return BuildVersion + BuildTypeString() + CurrentCommit
