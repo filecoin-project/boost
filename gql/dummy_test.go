daf5072da61039ecd3954da317da0be6f3d5ed05
LexLuthr <lexluthr@curiostorage.org>
2026-02-25 13:13:04 +0530

    fix flaxy gql test

--- gql/dummy_test.go
+++ gql/dummy_test.go
@@ -3,39 +3,54 @@ package gql
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
+	"net"
 	"net/http"
 	"os"
 	"path"
 	"strings"
-	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestDummyServer(t *testing.T) {
 	rq := require.New(t)
-	ctx := context.Background()
 
 	mux := http.NewServeMux()
-	port := 8080
-	listenAddr := fmt.Sprintf(":%d", port)
-	t.Logf("server listening on %s\n", listenAddr)
-	err := serveDummyDeals(mux, port)
+
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
 	rq.NoError(err)
 
-	srv := &http.Server{Addr: listenAddr, Handler: mux}
+	tcpAddr, ok := listener.Addr().(*net.TCPAddr)
+	rq.True(ok)
 
-	var wg sync.WaitGroup
-	wg.Add(1)
+	t.Logf("server listening on %s\n", listener.Addr().String())
+	err = serveDummyDeals(mux, tcpAddr.Port)
+	rq.NoError(err)
+
+	srv := &http.Server{Handler: mux}
+
+	serveErr := make(chan error, 1)
 	go func() {
-		defer wg.Done()
-
-		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
-			rq.Fail(err.Error())
+		if err := srv.Serve(listener); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			serveErr <- err
 		}
+		close(serveErr)
 	}()
+
+	t.Cleanup(func() {
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		require.NoError(t, srv.Shutdown(shutdownCtx))
+
+		if err, ok := <-serveErr; ok {
+			require.NoError(t, err)
+		}
+	})
 
 	var sourceBuff bytes.Buffer
 	sourceBuff.WriteString(strings.Repeat("test\n", 1024))
@@ -44,15 +59,17 @@ func TestDummyServer(t *testing.T) {
 	_, err = randBytes.ReadFrom(randSource)
 	rq.NoError(err)
 
-	fileName := "test.car"
-	err = os.WriteFile(path.Join(DummyDealsDir, fileName), randBytes.Bytes(), 0644)
+	fileName := fmt.Sprintf("%d-test.car", time.Now().UnixNano())
+	filePath := path.Join(DummyDealsDir, fileName)
+	err = os.WriteFile(filePath, randBytes.Bytes(), 0o644)
 	rq.NoError(err)
+	t.Cleanup(func() {
+		_ = os.Remove(filePath)
+	})
 
-	reqUrl := DummyDealsBase + "/" + fileName
-	req, err := http.NewRequest("GET", reqUrl, nil)
+	reqURL := fmt.Sprintf("http://%s/%s/%s", listener.Addr().String(), DummyDealsPrefix, fileName)
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, reqURL, nil)
 	rq.NoError(err)
-
-	req = req.WithContext(ctx)
 	resp, err := http.DefaultClient.Do(req)
 	rq.NoError(err)
 
@@ -68,10 +85,4 @@ func TestDummyServer(t *testing.T) {
 	rq.Equal(randBytes.String(), buf.String())
 
 	t.Logf("Read %d bytes", len(buf.Bytes()))
-
-	if err := srv.Shutdown(ctx); err != nil {
-		rq.Fail(err.Error())
-	}
-
-	wg.Wait()
 }
